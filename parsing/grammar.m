f073c26de4f1563d3bece7089dbdb8c436d1d465
ormanbai123 <lowliet1990@mail.ru>
2025-04-12 15:32:35 +0500

    Initial version of action code. Still incorrect

--- parsing/grammar.m
+++ parsing/grammar.m
@@ -3,12 +3,13 @@
 %symbol Session
 %symbol{} Statement Expr
 
-%symbol{} term def_specifier
-%symbol{logic::belief} struct_specifier
+%symbol{logic::term} term
+%symbol{logic::belief} struct_specifier def_specifier
 %symbol{std::stack<std::vector<std::pair<std::vector<std::string>, logic::type>>>} args_seq 
 %symbol{logic::type} type func 
 %symbol{std::vector<logic::type>} type_list
-%symbol{} iff_expr implication_expr or_expr and_expr not_expr quantifier_expr lazy_implication lazy_or lazy_and
+%symbol{logic::term} iff_expr implication_expr or_expr and_expr not_expr lazy_implication lazy_or lazy_and
+%symbol{} quantifier_expr // TODO change this.
 
 %symbol{std::vector<std::pair<std::vector<std::string>, logic::type>>} idents_type_list 
 %symbol{std::string} IDENTIFIER
@@ -26,7 +27,8 @@
 
 %symbol{ std::string } SCANERROR
 
-%symbol{} apply_args apply_expr member_apply_expr
+%symbol{} apply_args
+%symbol{logic::term} apply_expr member_apply_expr
 
 %symbol{} iff_expr_q implication_expr_q or_expr_q and_expr_q not_expr_q
 
@@ -65,7 +67,7 @@ Session =>
          ;
 
 Statement => struct_specifier : strct { blfs.append(std::move(strct)); }
-           | def_specifier
+           | def_specifier : def { blfs.append(std::move(def)); }
            ;
 
 idents_type_list => identifiers_colon_type:ict { return {ict}; }
@@ -111,7 +113,17 @@ struct_specifier => STRUCT IDENTIFIER:s ASSIGN idents_type_list:v
 
 //-----------------------defs---------------------------------
 
-def_specifier => DEF IDENTIFIER args_seq ASSIGN term {std::cout << "Definition!\n";};		  
+def_specifier => DEF IDENTIFIER:s args_seq ASSIGN term:trm {
+	std::cout << "Definition!\n";
+
+	// TODO change this.
+	using namespace logic;
+
+	auto T = type( type_truthval );
+	auto tp = type(type_func, T, {type(type_obj)});
+
+	return belief(bel_def, identifier() + s, trm, tp);
+};
 
 args_seq => args_seq:st LPAR idents_type_list:v RPAR {st.push(v); return st;}
 		  | LPAR idents_type_list:v RPAR {
@@ -125,71 +137,82 @@ args_seq => args_seq:st LPAR idents_type_list:v RPAR {st.push(v); return st;}
 
 //-----------------------terms---------------------------------
 
-term => quantifier_expr iff_expr
-      | iff_expr
+term => quantifier_expr iff_expr:trm { return trm; } // TODO change this.
+      | iff_expr:trm { return trm; }
       ;  
 
-iff_expr => iff_expr IFF quantifier_expr implication_expr
-          | iff_expr IFF implication_expr
-          | implication_expr 
+iff_expr => iff_expr:trm_left IFF quantifier_expr implication_expr:trm_right { return logic::term(logic::op_equiv, trm_left, trm_right); } // TODO change this
+          | iff_expr:trm_left IFF implication_expr:trm_right { return logic::term(logic::op_equiv, trm_left, trm_right); }
+          | implication_expr:trm { return trm; }
           ;
 
-implication_expr => or_expr IMPLY quantifier_expr implication_expr
-                  | or_expr IMPLY implication_expr
-                  | or_expr
+implication_expr => or_expr:trm_left IMPLY quantifier_expr implication_expr:trm_right { return logic::term(logic::op_implies, trm_left, trm_right); } // TODO change this.
+                  | or_expr:trm_left IMPLY implication_expr:trm_right { return logic::term(logic::op_implies, trm_left, trm_right); }
+                  | or_expr:trm { return trm; }
                   ;
 
-or_expr => or_expr OR quantifier_expr and_expr
-         | or_expr OR and_expr
-         | and_expr
+or_expr => or_expr:trm_left OR quantifier_expr and_expr:trm_right { return logic::term(logic::op_or, trm_left, trm_right); } // TODO change this
+         | or_expr:trm_left OR and_expr:trm_right { return logic::term(logic::op_or, trm_left, trm_right); }
+         | and_expr:trm { return trm; }
          ;
 
-and_expr => and_expr AND quantifier_expr lazy_implication
-          | and_expr AND lazy_implication
-          | lazy_implication 
+and_expr => and_expr:trm_left AND quantifier_expr lazy_implication:trm_right { return logic::term(logic::op_and, trm_left, trm_right); } // TODO change this
+          | and_expr:trm_left AND lazy_implication:trm_right { return logic::term(logic::op_and, trm_left, trm_right); }
+          | lazy_implication:trm { return trm; }
           ;
 
-lazy_implication => LBRACE term RBRACE IMPLY quantifier_expr lazy_or
-                  | LBRACE term RBRACE IMPLY lazy_or
-                  | lazy_or
+lazy_implication => LBRACE term:trm_left RBRACE IMPLY quantifier_expr lazy_or:trm_right { return logic::term(logic::op_lazy_implies, trm_left, trm_right); } // TODO change this
+                  | LBRACE term:trm_left RBRACE IMPLY lazy_or:trm_right { return logic::term(logic::op_lazy_implies, trm_left, trm_right); }
+                  | lazy_or:trm { return trm; }
                   ;
 
-lazy_or => LBRACE term RBRACE OR quantifier_expr lazy_or
-         | LBRACE term RBRACE OR lazy_or
-         | lazy_and
+lazy_or => LBRACE term:trm_left RBRACE OR quantifier_expr lazy_or:trm_right { return logic::term(logic::op_lazy_or, trm_left, trm_right); } // TODO change this
+         | LBRACE term:trm_left RBRACE OR lazy_or:trm_right { return logic::term(logic::op_lazy_or, trm_left, trm_right); }
+         | lazy_and:trm { return trm; }
          ;
 
-lazy_and => LBRACE term RBRACE AND quantifier_expr lazy_and
-         | LBRACE term RBRACE AND lazy_and
-         | not_expr
+lazy_and => LBRACE term:trm_left RBRACE AND quantifier_expr lazy_and:trm_right { return logic::term(logic::op_lazy_and, trm_left, trm_right); } // TODO change this
+         | LBRACE term:trm_left RBRACE AND lazy_and:trm_right { return logic::term(logic::op_lazy_and, trm_left, trm_right); }
+         | not_expr:trm { return trm; }
          ;
 
-not_expr => NOT not_expr 
-          | PROP not_expr
-          | member_apply_expr
-          | apply_expr
-          | NOT quantifier_expr not_expr
-          | PROP quantifier_expr not_expr
-          | LPAR term RPAR
+not_expr => NOT not_expr:trm { return logic::term( logic::op_not, trm ); }
+          | PROP not_expr:trm { return logic::term( logic::op_prop, trm ); }
+          | member_apply_expr:trm { return trm; }
+          | apply_expr:trm { return trm; }
+          | NOT quantifier_expr not_expr:trm { return trm; } // TODO change this
+          | PROP quantifier_expr not_expr:trm { return trm; } // TODO change this
+          | LPAR term:trm RPAR { return trm; }
           ;
 
-quantifier_expr => LBRACKET identifiers_colon_type RBRACKET
-                 | LT identifiers_colon_type GT
-                 | quantifier_expr LBRACKET identifiers_colon_type RBRACKET
-                 | quantifier_expr LT identifiers_colon_type GT
+// TODO change this
+// Probably should change the grammar
+quantifier_expr => LBRACKET identifiers_colon_type RBRACKET {}
+                 | LT identifiers_colon_type GT {}
+                 | quantifier_expr LBRACKET identifiers_colon_type RBRACKET {}
+                 | quantifier_expr LT identifiers_colon_type GT {}
                  ;
 
-apply_args => term COMMA apply_args
-            | term 
+apply_args => term COMMA apply_args {}
+            | term {}
             ;
 
-apply_expr => IDENTIFIER LPAR apply_args RPAR
-            | IDENTIFIER LPAR RPAR 
+apply_expr => IDENTIFIER:s LPAR apply_args RPAR {
+				// TODO Fix this
+				auto ident = logic::term(logic::op_unchecked, identifier() + s.c_str());
+				// return logic::term(logic::op_apply, ident);
+				return logic::term(logic::op_unchecked, identifier() + s.c_str());
+			}
+            | IDENTIFIER:s LPAR RPAR {
+				auto ident = logic::term(logic::op_unchecked, identifier() + s.c_str());
+				//return logic::term(logic::op_apply, ident);
+				return logic::term(logic::op_unchecked, identifier() + s.c_str());
+			}
             ; 
 
-member_apply_expr => member_apply_expr DOT IDENTIFIER
-                   | member_apply_expr DOT apply_expr
-                   | IDENTIFIER
+member_apply_expr => member_apply_expr:trm DOT IDENTIFIER { return trm; } // TODO fix this
+                   | member_apply_expr:trm DOT apply_expr { return trm; } // TODO fix this
+                   | IDENTIFIER:s { return logic::term(logic::op_unchecked, identifier() + s.c_str()); }
                    ;
 
 %end
