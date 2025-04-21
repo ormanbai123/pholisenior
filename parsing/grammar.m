aa0808ea9b442a5c80db885066c4af380a098f62
ormanbai123 <lowliet1990@mail.ru>
2025-04-21 15:21:13 +0500

    Finished action code for quantified expressions

--- parsing/grammar.m
+++ parsing/grammar.m
@@ -150,46 +150,62 @@ args_seq => args_seq:st LPAR idents_type_list:v RPAR {st.push(v); return st;}
 
 //-----------------------terms---------------------------------
 
-term => quantifiers: st iff_expr: trm { return QuantifiedTerm(st, trm); }
+term => quantifiers:qntf iff_expr: trm { return QuantifiedTerm(qntf, trm); }
 		  | iff_expr:trm { return trm; }
       ;  
 
 iff_expr => iff_expr:trm_left IFF implication_expr:trm_right { return logic::term(logic::op_equiv, trm_left, trm_right); }
-          | iff_expr:trm_left IFF quantifiers implication_expr:trm_right { return logic::term(logic::op_equiv, trm_left, trm_right); }
+          | iff_expr:trm_left IFF quantifiers:qntf implication_expr:trm_right {
+				return logic::term(logic::op_equiv, trm_left, QuantifiedTerm(qntf, trm_right));
+			}
           | implication_expr:trm { return trm; }
           ;
 
-implication_expr => or_expr:trm_left IMPLY quantifiers implication_expr:trm_right { return logic::term(logic::op_implies, trm_left, trm_right); }
+implication_expr => or_expr:trm_left IMPLY quantifiers:qntf implication_expr:trm_right {
+	return logic::term(logic::op_implies, trm_left, QuantifiedTerm(qntf, trm_right));
+}
 					        | or_expr:trm_left IMPLY implication_expr:trm_right { return logic::term(logic::op_implies, trm_left, trm_right); }
                   | or_expr:trm { return trm; }
                   ;
 
-or_expr => or_expr:trm_left OR quantifiers and_expr:trm_right { return logic::term(logic::op_or, trm_left, trm_right); }
+or_expr => or_expr:trm_left OR quantifiers:qntf and_expr:trm_right {
+	return logic::term(logic::op_or, trm_left, QuantifiedTerm(qntf, trm_right));
+}
 		     | or_expr:trm_left OR and_expr:trm_right { return logic::term(logic::op_or, trm_left, trm_right); }
          | and_expr:trm { return trm; }
          ;
 
-and_expr => and_expr:trm_left AND quantifiers lazy_implication:trm_right { return logic::term(logic::op_and, trm_left, trm_right); }
+and_expr => and_expr:trm_left AND quantifiers:qntf lazy_implication:trm_right {
+	return logic::term(logic::op_and, trm_left, QuantifiedTerm(qntf, trm_right));
+}
 		      | and_expr:trm_left AND lazy_implication:trm_right { return logic::term(logic::op_and, trm_left, trm_right); }
           | lazy_implication:trm { return trm; }
           ;
 
-lazy_implication => forall LBRACE term:trm_left RBRACE IMPLY lazy_or:trm_right { return logic::term(logic::op_lazy_implies, trm_left, trm_right); } // TODO change this
+lazy_implication => forall:qntf LBRACE term:trm_left RBRACE IMPLY lazy_or:trm_right {
+	return QuantifiedTerm(qntf, logic::term(logic::op_lazy_implies, trm_left, trm_right));
+}
                   | lazy_or:trm { return trm; }
                   ;
 
-lazy_or => exist LBRACE term:trm_left RBRACE OR lazy_or:trm_right { return logic::term(logic::op_lazy_or, trm_left, trm_right); } // TODO change this
+lazy_or => exist:qntf LBRACE term:trm_left RBRACE OR lazy_or:trm_right {
+	return QuantifiedTerm(qntf, logic::term(logic::op_lazy_or, trm_left, trm_right));
+}
          | lazy_and:trm { return trm; }
          ;
 
-lazy_and => exist LBRACE term:trm_left RBRACE AND lazy_and:trm_right { return logic::term(logic::op_lazy_and, trm_left, trm_right); } // TODO change this
+lazy_and => exist:qntf LBRACE term:trm_left RBRACE AND lazy_and:trm_right {
+	return QuantifiedTerm(qntf, logic::term(logic::op_lazy_and, trm_left, trm_right));
+}
          | not_expr:trm { return trm; }
          ;
 
 not_expr => NOT not_expr:trm { return logic::term( logic::op_not, trm ); }
           | PROP not_expr:trm { return logic::term( logic::op_prop, trm ); }
-		      | NOT quantifiers not_expr:trm { return trm; } // TODO finish action code.
-		      | PROP quantifiers not_expr:trm { return trm; } // TODO finish action code.
+		      | NOT quantifiers:qntf not_expr:trm {
+				return QuantifiedTerm(qntf, trm);
+				}
+		      | PROP quantifiers:qntf not_expr:trm { return QuantifiedTerm(qntf, trm); }
           | member_apply_expr:trm { return trm; }
           | apply_expr:trm { return trm; }
           | LPAR term:trm RPAR { return trm; }
